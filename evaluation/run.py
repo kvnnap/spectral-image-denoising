39bdfeeda8942b127bb6e1c1a4214c3e0050045a
Kevin Napoli <kvnnap@gmail.com>
2023-08-16 16:41:42 +0200

    Load configuration using JSON and add argument parsing

--- evaluation/run.py
+++ evaluation/run.py
@@ -1,13 +1,14 @@
 import sys
 import os
+import argparse
+import jsonpickle
+
 sys.path.append(os.getcwd())
 
 from metric import MetricFactory
 from thresholds import ThresholdFactory
 from search import SearchFactory
 from denoiser import *
-
-import jsonpickle
 
 class ParameterSpace:
     def __init__(self):
@@ -24,13 +25,24 @@ class RunResult:
         self.denoiserParams = denoiserParams
         self.denoiserResult = denoiserResult
 
+class Progress:
+    def __init__(self, fn):
+        self.callback = fn
+    def update(self, current, total):
+        self.callback(current, total)
+
 class Run:
-    def __init__(self, parameterSpace):
+    def __init__(self, parameterSpace, monitoringFn=lambda n,d: None):
         self.parameterSpace = parameterSpace
+        self.progress = Progress(monitoringFn)
         self.runs = []
+
+    # def monitorProgress(self, fn):
+    #     self.progress = Progress(fn)
     
     def run(self):
         p = self.parameterSpace
+        totalRuns = len(p.images) * len(p.metrics) * len(p.thresholds) * len(p.searchMethods) * len(p.coefficientLengths) * len(p.iterations) * len(p.denoisers)
         for denoiserName in p.denoisers:
             denoiserMethod = DenoiserFactory.create(denoiserName)
             for img in p.images:
@@ -48,8 +60,7 @@ class Run:
                                         denoiserParams = DenoiserRunParams((refImage, image), metricMethod, thresholdMethod, searchMethod, coeff, iteration)
                                         denoiserResult = denoiserMethod.run(denoiserParams)
                                         self.runs.append(RunResult(denoiserParams, denoiserResult))
-    
-
+                                        self.progress.update(len(self.runs), totalRuns)
 
 def save(file_name, obj):
         with open(file_name, 'w') as fp:
@@ -60,25 +71,37 @@ def load(file_name):
         return jsonpickle.decode(fp.read())
 
 def main():
-    ps = ParameterSpace()
-    ps.images = [('images/dice_caustics/output_0.raw', ['images/dice_caustics/output_1.raw'])]
-    ps.metrics = ['mse']
-    ps.thresholds = ['mult']
-    ps.searchMethods = ['naive', 'gp_minimize']
-    ps.coefficientLengths = [16]
-    ps.iterations = [10]
-    ps.denoisers = ['fourier']
+    parser = argparse.ArgumentParser(description="Evaluates denoising using the parameter space provided in the input json file")
+    parser.add_argument('--config', default='config.json', help='Input file path')
+    parser.add_argument('--result', default='result.json', help='Output file path')
+    args = parser.parse_args()
 
-    run = Run(ps)
+    configPath = args.config
+    resultPath = args.result
+    print(f'Reading ParameterSpace from \'{configPath}\' and writing result to \'{resultPath}\'')
+
+    parameterSpace = load(configPath)
+    run = Run(parameterSpace, lambda n, d: print(f'Progress: {n} out of {d}', end='\r'))
     run.run()
 
-    save('run.json', run)
+    save(resultPath, run)
 
-    run2 = load('run.json')
-
-    print(f"Result count: {len(run.runs)}")
+    print(f'\nResult count: {len(run.runs)}')
 
 # The following code block will only execute if this script is run directly,
 # not if it's imported as a module in another script.
 if __name__ == "__main__":
     main()
+
+# def to_dict(obj):
+#     if isinstance(obj, list):
+#         out = list(map(lambda x: to_dict(x), obj))
+#     elif isinstance(obj, tuple):
+#         out = tuple(map(lambda x: to_dict(x), obj))
+#     elif (hasattr(obj, '__dict__')):
+#         out = {}
+#         for attr_name, attr_value in obj.__dict__.items():
+#             out[attr_name] = to_dict(attr_value)
+#     else:
+#         out = obj
+#     return out
