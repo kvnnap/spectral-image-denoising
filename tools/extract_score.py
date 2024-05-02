2486d9d8bcfdba4a0ea5f8fdb04dc3b12ab91e09
Kevin Napoli <kvnnap@gmail.com>
2024-05-02 03:14:53 +0200

    Update extract score for best metrics

--- tools/extract_score.py
+++ tools/extract_score.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.versioning import get_version
 from utils.serialisation import load, save, save_text
 from utils.string import comma_to_list, get_prefix, tables_to_csv, tables_to_latex
-from utils.constants import NAMEMAP, CONV
+from utils.constants import METRICS, NAMEMAP, CONV
 
 from visualisation.row_data import RowData
 
@@ -23,8 +23,8 @@ def main():
     parser.add_argument('--result', default='offline/smb/exp_1b/result_gray_all_merged.json', help='The result to load')
     parser.add_argument('--image-loaders', default='gray_aces_tm_nogamma', help='Which image loaders will be considered (comma separated) (tabbed)')
     parser.add_argument('--metrics', default='', help='Metrics (comma separated) (tabbed)')
-    parser.add_argument('--use-best-metrics', default=False, action='store_true', help='Sort by best metrics. There will be no filtering occuring using one metric')
-    parser.add_argument('--out-table', default='offline/smb/exp_1c/config_top_runs_gray_aces_tm_ng.csv', help='The config produced for running the samples')
+    parser.add_argument('--use-best-metrics', default=True, action='store_true', help='Sort by best metrics. There will be no filtering occuring using one metric')
+    parser.add_argument('--out-table', default='offline/smb/exp_1c/config_top_runs_gray_aces_tm_ng_bm.csv', help='The config produced for running the samples')
     args = parser.parse_args()
 
     print(f'Loading {args.result} and saving top to {args.out_table}')
@@ -35,12 +35,17 @@ def main():
         'ref-noisy': []
     }
 
+    scoreIndex = RowData.HEADER.index('score')
+    sceneIndex = RowData.HEADER.index('ref-noisy')
+    metricIndex = RowData.HEADER.index('metric')
+    denIndex = RowData.HEADER.index('denoiser_coeff')
+    thresIndex = RowData.HEADER.index('thresholding')
+
     rowData = RowData(runData)
     if args.use_best_metrics:
         rowData.sort_row_data_metric()
     else:
         catToVary['metric'] = comma_to_list(args.metrics)
-        scoreIndex = RowData.HEADER.index('score')
         rowData.sort_row_data(scoreIndex)
     
     filterDict = rowData.get_filter_dict()
@@ -58,29 +63,50 @@ def main():
     rDict = {r.denoiserParams.id: r for r in runData.runs}
     runData.runs = [rDict[fRow[0]] for fRow in filtered]
     
+    # rowData = RowData(runData)
+    # filterDict = rowData.get_filter_dict()
+
     tables = []
+    
     # Generate summary table
     table = []
-    header = ['scene', *filterDict['metric']]
-    table.append(header)
-    sceneIndex = RowData.HEADER.index('ref-noisy')
-    metricIndex = RowData.HEADER.index('metric')
-    for scene in filterDict['ref-noisy']:
-        #sRuns = [r for r in runData.runs if r.denoiserParams.]
-        sceneName = get_prefix(scene.split('-')[0], -2)
-        mappedName = NAMEMAP[sceneName]
+    if args.use_best_metrics:
+        header = ['scene', *METRICS]
+        table.append(header)
 
-        sceneRowData = [r for r in filtered if r[sceneIndex] == scene]
-        data = []
-        for metric in filterDict['metric']:
-            row = [r for r in sceneRowData if r[metricIndex] == metric]
-            data.append(CONV[metric](row[0][scoreIndex]))
-        table.append([mappedName, *data])
+        for scene in filterDict['ref-noisy']:
+            sceneName = get_prefix(scene.split('-')[0], -2)
+            mappedName = NAMEMAP[sceneName]
+
+            # get actual run
+            sceneRowDatum = [r for r in filtered if r[sceneIndex] == scene][0]
+            run = rDict[sceneRowDatum[0]]
+
+            data = []
+            for metric in METRICS:
+                score = run.bestMetricResults[metric][1]
+                data.append(CONV[metric](score))
+            table.append([mappedName, *data])
+    else:
+        header = ['scene', *filterDict['metric']]
+        table.append(header)
+        
+        for scene in filterDict['ref-noisy']:
+            #sRuns = [r for r in runData.runs if r.denoiserParams.]
+            sceneName = get_prefix(scene.split('-')[0], -2)
+            mappedName = NAMEMAP[sceneName]
+
+            sceneRowData = [r for r in filtered if r[sceneIndex] == scene]
+            data = []
+            for metric in filterDict['metric']:
+                row = [r for r in sceneRowData if r[metricIndex] == metric]
+                data.append(CONV[metric](row[0][scoreIndex]))
+            table.append([mappedName, *data])
+
     tables.append(table)
 
     # Generate detailed tables
-    denIndex = RowData.HEADER.index('denoiser_coeff')
-    thresIndex = RowData.HEADER.index('thresholding')
+    
     header = ['scene', 'metric', 'score', 'denoiser', 'threshold']
     table = [ header ]
     for scene in filterDict['ref-noisy']:
@@ -112,8 +138,6 @@ def main():
     latexStr = tables_to_latex(tables)
     latexPath = str(Path(path).with_suffix('.tex'))
     save_text(latexPath, latexStr)
-
-
 
 # The following code block will only execute if this script is run directly,
 # not if it's imported as a module in another script.
