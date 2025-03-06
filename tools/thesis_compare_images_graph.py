cd33ccaa729162364c01a8178372849401453a95
Kevin Napoli <kvnnap@gmail.com>
2025-03-06 16:33:10 +0100

    make thesis compare images script more configurable

--- tools/thesis_compare_images_graph.py
+++ tools/thesis_compare_images_graph.py
@@ -13,12 +13,11 @@ import sys
 import os
 import argparse
 import re
+import tqdm
 import matplotlib.pyplot as plt
+
 from pathlib import Path
 from collections import defaultdict
-
-import tqdm
-
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -26,15 +25,28 @@ from evaluation.image_loader import ImageLoaderFactory
 from evaluation.metric import *
 from utils.serialisation import load, save, save_text
 from utils.image import set_base_path
+from utils.string import comma_to_list
 
 # Globals
-imageLoaderStr = 'rgb_aces_tm_nogamma'
+
 # imageBufferTypes = ["diff_acc", "spec_acc", "caus_acc", "rad_acc"]
-imageLoader = ImageLoaderFactory.create(imageLoaderStr)
-file_pattern = r"^(?P<name>.*)_(?P<shader>pt|lt|lt_opt|lt_imp_dist|lt_imp_dist_cam|lt_imp_dist_cam_rt|ref)_(?P<buffer_type>diff_acc|spec_acc|caus_acc|rad_acc)_(?P<sequence_number>\d+)(?:\.(?P<extension>\w+))?$"
-shader_pattern = r"^(pt|lt|lt_opt|lt_imp_dist|lt_imp_dist_cam|lt_imp_dist_cam_rt|ref)$"
+imageLoaderStr = None
+imageLoader = None
+file_pattern = None
+shader_pattern = None
+metric_pattern = None
+buffer_pattern = None
+
 ref_name = 'ref'
 ref_pattern = fr"^({ref_name})$"
+
+metric_fns = {
+    'flip':      lambda ref, noisy, dp :  local_flip(ref, noisy, dp),
+    'hdrvdp3':   lambda ref, noisy, dp : -local_hdrvdp3(ref, noisy, dp),
+    'mse':       lambda ref, noisy, dp :  local_mse(ref, noisy, dp),
+    'psnr':      lambda ref, noisy, dp : -local_psnr(ref, noisy, dp),
+    'ssim':      lambda ref, noisy, dp : -local_ssim(ref, noisy, dp),
+}
 
 class DPString:
     def __init__(self, imageLoader):
@@ -42,23 +54,14 @@ class DPString:
 
 def compute_score(ref, noisy, imgLoaderStr):
     dp = DPString(imgLoaderStr)
-    ret_obj = {
-        'flip': local_flip(ref, noisy, dp),
-        'hdrvdp3': -local_hdrvdp3(ref, noisy, dp),
-        'mse': local_mse(ref, noisy, dp),
-        'psnr': -local_psnr(ref, noisy, dp),
-        'ssim': -local_ssim(ref, noisy, dp)
-    }
+    ret_obj = {}
+    for metric_name, similarity_fn in metric_fns.items():
+        ret_obj[metric_name] = similarity_fn(ref, noisy, dp)
     return ret_obj
 
 def compare_images(ref_data, noisy_image):
-    # Load images
     noisy_data = imageLoader(noisy_image['image'])
-
-    # Compare images
-    # score_ref = compute_score(ref_data, ref_data, imageLoaderStr)
     score = compute_score(ref_data, noisy_data, imageLoaderStr)
-
     return score
 
 # Get list of directories in the results directory using pathlib
@@ -86,13 +89,14 @@ def scan_directory(results_dir):
                     if match.group('extension').lower() != 'exr':
                         continue
 
-                    image_counter += 1
                     # Concat image object with match.groupdict()
                     my_image = { 'image': str(image.relative_to(results_dir)) }
                     # my_image = {'image': image }
                     my_image.update(match.groupdict())
                     my_image['sequence_number'] = int(match.group('sequence_number'))
                     my_images.append(my_image)
+                    if ref_name != shader.name:
+                        image_counter += 1
 
             # Generate map of images by buffer type and shader type
             my_images.sort(key=lambda x: x['sequence_number'])
@@ -118,7 +122,6 @@ def compute_scores(g_image_map, image_counter):
 
                 ref_image = images[-1]
                 ref_data = imageLoader(ref_image['image'])
-                bar.update(1)
 
                 for shader, img_list in shaders_image_map.items():
                     if shader == ref_name:
@@ -139,12 +142,17 @@ def plot_scores(g_image_map, out_dir):
     for scene, cameras in g_image_map.items():
         for camera, image_map in cameras.items():
             for buff_type, shaders_image_map in image_map.items():
-                metrics = {}
+                if not re.match(buffer_pattern, buff_type):
+                    continue
+                metrics = set()
                 for shader, images in shaders_image_map.items():
+                    if not re.match(shader_pattern, shader):
+                        continue
                     for img in images:
                         if 'score' not in img: continue
                         for metric, score in img['score'].items():
-                            metrics[metric] = True
+                            if re.match(metric_pattern, metric):
+                                metrics.add(metric)
                 num_plots += len(metrics)
 
     plt.ioff()
@@ -154,17 +162,21 @@ def plot_scores(g_image_map, out_dir):
             specific_dir = plots_dir.joinpath(f'{scene}-{camera}')
             specific_dir.mkdir(parents=True, exist_ok=True)
             for buff_type, shaders_image_map in image_map.items():
-
+                if not re.match(buffer_pattern, buff_type):
+                    continue
                 # Get Scores for this scene, camera, and buffer type
                 seqs = defaultdict(list)
                 scores = defaultdict(lambda: defaultdict(list))
                 for shader, images in shaders_image_map.items():
+                    if not re.match(shader_pattern, shader):
+                        continue
                     for img in images:
                         if 'score' not in img:
                             continue  # Skip images without scores
                         seqs[shader].append(img['sequence_number'])
                         for metric, score in img['score'].items():
-                            scores[metric][shader].append(score)
+                            if re.match(metric_pattern, metric):
+                                scores[metric][shader].append(score)
                 
                 # Plot data for each metric, one plot per metric
                 for metric, shaders in scores.items():
@@ -193,10 +205,36 @@ def main():
     parser = argparse.ArgumentParser(description=f'Compare images and generate plots for each scene, camera, buffer type and metric combination.')
     parser.add_argument('--results-dir', type=str, required=True, help='Path to the Results directory containing images for comparison.')
     parser.add_argument('--output-dir', type=str, default='plots', help='Directory to save the output plot and scores to.')
+    parser.add_argument('--image-loader', type=str, default='rgb_aces_tm_nogamma', help='Which loader to use when loading EXR images.')
     parser.add_argument('--scores-file', type=str, default='scores.json', help='Name of the scores JSON file to load/save.')
     parser.add_argument('--plot-only', action='store_true', help='If set, only plot the graph without recomputing scores.')
+    # List-based
+    parser.add_argument('--metrics', default='flip,hdrvdp3,mse,psnr,ssim', help='The metric name to use for comparison. Default all metrics.')
+    parser.add_argument('--buffer-types', default='diff_acc,spec_acc,caus_acc,rad_acc', help='The buffer types to use for comparison. Defaults to all buffer types.')
+    parser.add_argument('--shaders', default='pt,lt,lt_opt,lt_imp_dist,lt_imp_dist_cam,lt_imp_dist_cam_rt', help='The shaders to use for comparison. Defaults to all shaders.')
 
     args = parser.parse_args()
+
+    global imageLoaderStr, imageLoader, file_pattern, shader_pattern, metric_pattern, buffer_pattern, metric_fns
+    imageLoaderStr = args.image_loader
+    imageLoader = ImageLoaderFactory.create(imageLoaderStr)
+
+    metrics = comma_to_list(args.metrics)
+    buffer_types = comma_to_list(args.buffer_types)
+    shaders = comma_to_list(args.shaders)
+
+    if not(metrics and buffer_types and shaders):
+        print("Please specify at least one buffer type, shader, and metric.")
+        parser.print_help()
+        return
+
+    file_pattern = fr"^(?P<name>.*)_(?P<shader>{'|'.join(shaders)}|ref)_(?P<buffer_type>{'|'.join(buffer_types)})_(?P<sequence_number>\d+)(?:\.(?P<extension>\w+))?$"
+    shader_pattern = fr"^({'|'.join(shaders)}|ref)$"
+    buffer_pattern = fr"^({'|'.join(buffer_types)})$"
+    metric_pattern = fr"^({'|'.join(metrics)})$"
+    
+    # Remove all metric functions not matching the metric_pattern
+    metric_fns = {metric: metric_fn for metric, metric_fn in metric_fns.items() if re.match(metric_pattern, metric) }
 
     results_dir = Path(args.results_dir)
     output_dir  = Path(args.output_dir)
